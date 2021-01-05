a558443f9fe9510de1408fb8f44603319e8b661e
trinh6299 <trinh6299@gmail.com>
2021-01-05 20:38:15 +0700

    finallllll

--- Web_Music/GUI/Home.aspx.cs
+++ Web_Music/GUI/Home.aspx.cs
@@ -12,20 +12,22 @@ namespace Web_Music.GUI
     public partial class Home : System.Web.UI.Page
     {
         BUS_Multitable bus_multitable = new BUS_Multitable();
+        BUS_Song bus_song = new BUS_Song();
         public DataTable dt1 = new DataTable();
         public DataTable dt2 = new DataTable();
         public DataTable dt3 = new DataTable();
         public DataTable dt4 = new DataTable();
         public DataTable dt5 = new DataTable();
         public DataTable dt6 = new DataTable();
+        public DataTable dt7 = new DataTable();
         protected void Page_Load(object sender, EventArgs e)
         {
             //Lấy các bài trong album nghe gì hôm nay gán vào dt1
             //dt1 = bus_multitable.GetDataByAlbumName("Nghe gì hôm nay");
             dt1 = bus_multitable.GetDataByAlbumId(5);
 
-            //Lấy các bài trong album nghe gì hôm nay gán vào dt2
-            dt2 = bus_multitable.GetDataByAlbumName("Nghe gì hôm nay");
+            //Lấy các bài trong HOT hôm nay gán vào dt2
+            dt2 = bus_multitable.GetDataByAlbumId(17);
 
             //Lấy các bài hát trong album Việt Nam Top 10 gán vào dt3
             dt3 = bus_multitable.GetDataByAlbumId(23);
@@ -38,6 +40,9 @@ namespace Web_Music.GUI
 
             //Lấy các bài hát trong album Hàn Quốc Top 10 gán vào dt6
             dt6 = bus_multitable.GetDataByAlbumId(27);
+
+            //Lấy random các bài hát gán vào dt7
+            dt7 = bus_song.GetAllChiTietBaiHatRandom();
         }
     }
 }
